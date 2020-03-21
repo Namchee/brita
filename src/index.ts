4dfaaa884d3140abb590c0a4bf2aadd54f93d073
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-03-21 08:27:25 +0700

    fix: Heroku H10 fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Koa from 'koa';
 import { createApp } from './app';
-import config from './config/env';
+// import config from './config/env';
 
 createApp().then((app: Koa) => {
-  const port = config.port;
+  const port = process.env.PORT || 8080;
 
   app.listen(port, () => {
     console.log(`App listening on ${port}`);
